d89f5cf75eb51f500d22294226df87548c6470c5
0x1DOCD00D <markgrechanik@gmail.com>
2025-05-19 14:21:04 -0500

    bindings and names 1

---
+++ src/main/java/BindingsNames.java
@@ -0,0 +1,7 @@
+public class BindingsNames {
+    int field = 5;
+    
+    public static void main(String[] args) {
+        System.out.println(new BindingsNames().field);
+    }
+}
---
+++ src/main/scala/BindingFuncNames.scala
@@ -0,0 +1,7 @@
+object BindingFuncNames {
+  val field:Int = 5
+
+  def main(args: Array[String]): Unit = {
+    println(field)
+  }
+}
