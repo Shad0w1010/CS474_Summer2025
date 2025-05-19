7446e612a7f419de2c25dbe7277280e94e955a1b
0x1DOCD00D <markgrechanik@gmail.com>
2025-05-19 15:24:28 -0500

    inc and dec

---
+++ src/main/java/IncAndDec.java
@@ -0,0 +1,10 @@
+public class IncAndDec {
+    public static void main(String[] args) {
+        int x = 0;
+        int y =2;
+        int z = 3;
+        
+        int result = x++ + --y + ++z;
+        System.out.println(result);
+    }
+}
