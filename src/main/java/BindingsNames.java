42fb29acd8f47f41c7b79959385cccffef2d3f3c
0x1DOCD00D <markgrechanik@gmail.com>
2025-05-19 15:22:47 -0500

    bindings and names and statics

--- src/main/java/BindingsNames.java
+++ src/main/java/BindingsNames.java
@@ -1,7 +1,12 @@
 public class BindingsNames {
     int field = 5;
     
+    public void setField(int p) {
+        field = p;
+    }
     public static void main(String[] args) {
+        int varrr= 10;
+        new BindingsNames().setField(varrr);
         System.out.println(new BindingsNames().field);
     }
 }
--- src/main/scala/BindingFuncNames.scala
+++ src/main/scala/BindingFuncNames.scala
@@ -1,7 +1,9 @@
 object BindingFuncNames {
   val field:Int = 5
+  
+  val mital:3 = 3
 
   def main(args: Array[String]): Unit = {
-    println(field)
+    println(mital)
   }
 }
