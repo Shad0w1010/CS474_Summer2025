d5d443c98ffb0bd898bb477b70ef01d0a96609c0
0x1DOCD00D <markgrechanik@gmail.com>
2025-05-27 14:45:05 -0500

    same sig methods

--- src/main/java/Intro2Interfaces.java
+++ src/main/java/Intro2Interfaces.java
@@ -5,6 +5,15 @@ public class Intro2Interfaces {
 //        int i;
         void f(); }
     interface B extends A { void g(); }
+    interface Im1 {void m();}
+    interface Im2 {void m();}
+    
+    class Imm implements Im1, Im2{
+        @Override
+        public void m() {
+            System.out.println("Imm::m");
+        }
+    }
     
     class C implements Empty{
         void f(){
@@ -15,6 +24,7 @@ public class Intro2Interfaces {
     
     public static void main(String[] args) {
         System.out.println("Intro to Interfaces");
+        new Intro2Interfaces().new Imm().m();
         new A() { public void f() { System.out.println("A::f"); } }.f();
         Empty e = new Intro2Interfaces().new C();
         ((C)e).f();
