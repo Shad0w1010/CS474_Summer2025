3f58d416e409b19184337004ff7645f12d8022b6
0x1DOCD00D <markgrechanik@gmail.com>
2025-06-02 15:28:15 -0500

    applyFN

--- src/main/scala/BasicLambdaFromLec7.scala
+++ src/main/scala/BasicLambdaFromLec7.scala
@@ -8,6 +8,17 @@ object BasicLambdaFromLec7 {
     override def apply(v1: Int): Int = ff(v1)
   }
 
+  def applyFuncNtimes(n: Int, f:Int=>Int): Int=>Int = {
+    require(n>=0)
+    if n == 0 then (x:Int)=>x else (i:Int)=>applyFuncNtimes(n-1,f)(f(i))
+  }
+  // applyFuncNtimes(3, x+1)
+//  s1: i=>applyFuncNtimes(2,x+1)f(i)
+//  s2: i=>(i=>applyFuncNtimes(1,x+1)f(i)))f(i))
+//  s3: i=>(i=>(i=>applyFuncNtimes(0,x+1)f(i))f(i)))f(i))
+//  s4: i=>(i=>(i=>((x:Int)=>x) f(i))f(i)))f(i))
+//
+
   val fff: Int => Int = (i:Int)=> _f(ff(i))
   def main(args: Array[String]): Unit = {
     println(lambdaExp(x=>x+10)(2))
@@ -16,5 +27,6 @@ object BasicLambdaFromLec7 {
     println(fff(2))
     val d = new DoubleApp
     println(d(2))
+    println("fN: " + applyFuncNtimes(3, x=>x+1)(10))
   }
 }
