d08cfeb2503b933dfed57d80562a6a1a26b2a0e3
0x1DOCD00D <markgrechanik@gmail.com>
2025-06-05 15:28:06 -0500

    exceptions and options

--- src/main/scala/TotalityAndPartiality.scala
+++ src/main/scala/TotalityAndPartiality.scala
@@ -1,7 +1,20 @@
 object TotalityAndPartiality {
 
-  def mean(lst:List[Float]): Float =
-    lst.sum/lst.length
+  trait Maybe[+T]
+  case object Bupkis extends Maybe[Nothing]
+  case class Perfect[T](v:T) extends Maybe[T]
+  
+  def mean(lst:List[Float]): Maybe[Float] = {
+    if lst.isEmpty then Bupkis else Perfect(lst.sum/lst.length)
+  }  
+  
+  def meanO(lst:List[Float]): Option[Float] = {
+    if lst.isEmpty then None else Some(lst.sum/lst.length)
+  }
+
+  def meanE(lst: List[Float]): Either[String, Float] = {
+    if lst.isEmpty then Left("You cannot divide by zero") else Right(lst.sum / lst.length)
+  }
 
   class MeanComputation extends PartialFunction[List[Float], Float] {
     override def isDefinedAt(x: List[Float]): Boolean = if x.length == 0 then false else true
@@ -12,6 +25,7 @@ object TotalityAndPartiality {
   def main(args: Array[String]): Unit = {
     println(mean(List(5.0f, 7.0f, 10.0f)))
     println(mean(List()))
+    println(meanE(List()))
   }
 
 }
